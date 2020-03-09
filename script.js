e1a911b5ca89e0cff1fc8cfc30025ee06191c43f
Polinarka <saira6303@gmail.com>
2020-03-09 10:57:18 +0400

    поправила файл js, исправила появление модального окна

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ console.log(button);
 var modalForm = document.querySelector(".modal-form_js");
 console.log(modalForm);
 var buttonClose = document.querySelector(".button-closed_js");
+var input = document.querySelector(".form-input_js");
 
 button.addEventListener("click", function () {
     modalForm.classList.remove("modal-form_none");
+    input.focus();
+    console.log();
 });
 
 buttonClose.addEventListener("click", function () {
     modalForm.classList.add("modal-form_none");
 });
+
+window.addEventListener("keydown" , function (event) {
+    if(event.code == "Escape");
+    modalForm.classList.add("modal-form_none");
+    console.log(event);
+});
+
